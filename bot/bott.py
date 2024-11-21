e3c4a80cdb2d92822fe6cef4f447ebf8a817290b
Polina Bychkova <polinapetcat@gmail.com>
2024-11-21 21:20:39 +0300

    база коммит

--- bot/bott.py
+++ bot/bott.py
@@ -1,1 +1,23 @@
-print(123)
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# Функция для обработки команды /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_name = update.effective_user.first_name
+    await update.message.reply_text(f'Привет, {user_name}! Рад тебя видеть!')
+
+def main():
+    # Вставьте свой токен Telegram API сюда
+    TOKEN = "7594370282:AAGpyh78Cr9TXqWyxYlBBJDv_BN34V2e5Jw"
+
+    # Создаем приложение
+    application = ApplicationBuilder().token(TOKEN).build()
+
+    # Регистрируем обработчик команды /start
+    application.add_handler(CommandHandler("start", start))
+
+    # Запускаем бота
+    application.run_polling()
+
+if __name__ == "__main__":
+    main()
