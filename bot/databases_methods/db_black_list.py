6e750d0ead4eec520537ae06cf529f5256cdd6e3
LizaBaranova239 <elsebaranova@edu.hse.ru>
2024-12-17 16:54:23 +0300

    блокировка работает

--- bot/databases_methods/db_black_list.py
+++ bot/databases_methods/db_black_list.py
@@ -5,6 +5,9 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE_DIR = os.path.join(BASE_DIR, "../databases")
 db_path = os.path.join(DATABASE_DIR, "black_list.db")
 
+DATABASE_DIR2 = os.path.join(BASE_DIR, "../databases")
+db_path2 = os.path.join(DATABASE_DIR2, "users.db")
+
 # Создание и инициализация базы данных
 def init_db_black_list():
     conn = sqlite3.connect(db_path)
@@ -23,7 +26,7 @@ def create_block(user_id: int, blocked_user_link: str):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    cursor.execute("ATTACH DATABASE 'users.db' AS users_db")
+    cursor.execute(f"ATTACH DATABASE '{db_path2}' AS users_db")
 
     cursor.execute("""
         SELECT UserId
@@ -31,11 +34,15 @@ def create_block(user_id: int, blocked_user_link: str):
         WHERE TelegramLink = ?
     """, (blocked_user_link,))
 
-    blocked_user_id = cursor.fetchone()[0]
+    result = cursor.fetchone()
+    if result is None:
+        print("Пользователь не найден.")
+        return
+
+    blocked_user_id = result[0]
 
     cursor.execute("""
         INSERT INTO black_list (UserId, BlockedUserId) VALUES (?, ?)
     """, (user_id, blocked_user_id))
     conn.commit()
     conn.close()
-
