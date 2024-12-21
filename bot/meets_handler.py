44cecab8e3262a5814e736185d46640d47817545
Liza Baranova <elsebaranova@edu.hse.ru>
2024-12-21 18:48:06 +0300

    Удаление встреч работает ураураура

--- bot/meets_handler.py
+++ bot/meets_handler.py
@@ -1,10 +1,12 @@
 import re
 from telegram import (Update, ReplyKeyboardMarkup, ReplyKeyboardRemove)
 from telegram.ext import CommandHandler, MessageHandler, ConversationHandler, ContextTypes, filters, CallbackContext, MessageHandler, ContextTypes, ConversationHandler, MessageHandler, CommandHandler, filters
-from bot.google_calendar.google_calendar import get_credentials, build
+from bot.google_calendar.google_calendar import get_credentials, build, delete_event
 from bot.databases_methods.db_user import get_email_by_user, get_user_by_email
 from datetime import datetime
 from googleapiclient.discovery import build
+
+DELETE_MEET = range(1)
 
 async def meets(update: Update, context: CallbackContext):
     user = update.message.from_user
@@ -74,7 +76,80 @@ async def edit_meet(update: Update, context: CallbackContext):
 
 async def delete_meet(update: Update, context: CallbackContext):
     user = update.message.from_user
-    await update.message.reply_text("Выберите встречу для удаления...")  # Пример ответа
+    user_email = get_email_by_user(user.username)
+    if not user:
+        await update.message.reply_text(f"Пользователь с email {user_email} не найден в базе данных.")
+        return ConversationHandler.END
+
+    user = get_user_by_email(user_email)
+    user_id = user[0]
+    calendar_id = user[3]  # GoogleCalendarLink
+
+    if not calendar_id:
+        await update.message.reply_text(f"Вы не привязали календарь, сначала привяжите его. \start")
+        return ConversationHandler.END
+
+    creds = get_credentials(user_id)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' обозначает UTC время
+    events_result = service.events().list(calendarId=calendar_id, timeMin=now, maxResults=10, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        await update.message.reply_text(f"У вас нет предстоящих событий")
+        return ConversationHandler.END
+
+    events_info = []
+    for event in events:
+        event_id = event['id']
+        event_summary = event.get('summary', 'Без названия')
+        event_start = event['start'].get('dateTime', event['start'].get('date'))
+        events_info.append((event_id, event_summary, event_start))
+
+    res = events_info
+
+    ans = "Вот список предстоящих событий:\n"
+    i = 1
+    for event_id, summary, start in res:
+        start_dt = datetime.fromisoformat(start[:-6])
+        start2 = start_dt.strftime("%d %B %Y %H:%M")
+        ans += f'''{i}. "{summary}"\n Начало: {start2}\n\n'''
+        i += 1
+    ans += "\n Выберите встречу для удаления, введите её название:"
+    await update.message.reply_text(ans)
+    return DELETE_MEET
+
+# Другая функция для обработки ID события
+async def handle_delete_meet(update: Update, context: CallbackContext):
+    user = update.message.from_user
+    user_email = get_email_by_user(user.username)
+    event_name = update.message.text
+    event_id = -1
+
+    user = get_user_by_email(user_email)
+    user_id = user[0]
+    calendar_id = user[3]
+
+    creds = get_credentials(user_id)
+    service = build('calendar', 'v3', credentials=creds)
+    now = datetime.utcnow().isoformat() + 'Z'  # 'Z' обозначает UTC время
+    events_result = service.events().list(calendarId=calendar_id, timeMin=now, singleEvents=True,
+                                          orderBy='startTime').execute()
+    events = events_result.get('items', [])
+    for event in events:
+        event_summary = event.get('summary', 'Без названия')
+        if event_summary == event_name:
+            event_id = event['id']
+
+    if event_id == -1 :
+        await update.message.reply_text(f'''Событие с названием "{event_name}" не найдено. Введите название повторно. Или отмените действие /cancel.''')
+        return DELETE_MEET
+
+    success, message = delete_event(user_email, event_id)
+    await update.message.reply_text(message)
     return ConversationHandler.END
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
@@ -91,7 +166,7 @@ def get_meet_handler():
                       CommandHandler("delete_meet", delete_meet),
                       ],
         states={
-
+            DELETE_MEET: [MessageHandler(filters.TEXT & ~filters.COMMAND, handle_delete_meet)],
         },
     fallbacks=[CommandHandler("cancel", cancel)],
 )
